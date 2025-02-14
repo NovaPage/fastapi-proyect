5b4703203a3d340bac711d49dea217a0522274f1
H <perezcastro.1228@gmail.com>
2025-02-14 16:08:10 -0500

    Se agrego una autenticacion basica

--- app/main.py
+++ app/main.py
@@ -1,10 +1,14 @@
+from http.client import HTTPException
 import time
+from typing import Annotated
 from urllib.request import Request
-from fastapi import FastAPI  # type: ignore
+from fastapi import FastAPI, Depends, HTTPException, status  # type: ignore
 from datetime import datetime
 from models import Invoice
 from db import create_all_tables
 from .routers import customers, transactions, plans
+from fastapi.security import HTTPBasic, HTTPBasicCredentials # type: ignore
+from typing import Annotated
 
 import zoneinfo
 
@@ -26,9 +30,16 @@ async def log_request_time(request: Request, call_next):
     print(f"Request: {request.url} completed in: {process_time:.4f} seconds")
     return response
 
+security = HTTPBasic()
+
 @app.get("/")
-async def root():
-    return {"message": "NovaPage"}
+async def root(credentials: Annotated[HTTPBasicCredentials, Depends(security)]):
+    print(credentials)
+    if credentials.username == "H" and credentials.password == "1228":
+        return {"message": f"Hola, {credentials.username}!"}
+    else:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED)
+
 
 country_timezones = {
     "CO": "America/Bogota",
@@ -41,8 +52,6 @@ async def get_time_by_iso_code(iso_code: str):
     timezone_str = country_timezones.get(iso)
     tz = zoneinfo.ZoneInfo(timezone_str)
     return {"time": datetime.now(tz)}
-
-# db_customers: list[Customer] = []
 
 @app.post("/Invoices")
 async def create_invoices(invoices_data: Invoice):
